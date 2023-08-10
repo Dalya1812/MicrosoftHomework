dd00e859fc8ebf782185021446c33c94bb548601
Dalia <dalya1812@gmail.com>
2023-08-10 08:56:28 +0300

    create a functions of arm templates

--- vm_deployment.ps1
+++ vm_deployment.ps1
@@ -1,20 +1,39 @@
 Connect-AzAccount
-
-#$templateFilePath= "./vm_template.json"
-#$deployment =New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $templateFilePath -vmName "myNewVM"
-#$deploymentName = $deployment.DeploymentName
-#$deployment.ProvisioningState
-#$deployment.Error
-
-
-$templateFilePath= "./storage_account_template.json"
-$storageAccountName = "mystor5435345"
-$resourceGroupName = "NewResourceGroup"
 $location = "East US"
+$resourceGroupName = "NewResourceGroup"
 $sku = "Standard_LRS"
 
-New-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $storageAccountName -Location $location -SkuName $sku
-$deployment.ProvisioningState
-$deployment.Error
+
+function Create-ResourceGroup{
+    param (
+        [string]$resourceGroupName,
+        [string]$location,
+        [string]$sku
+    )
+    $templateFilePath ="./vm_template.json"
+    $deployment =New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $templateFilePath -vmName "myNewVM"
+    $deploymentName = $deployment.DeploymentName
+    $deployment.ProvisioningState
+    $deployment.Error
+}
+
+
+
+
+
+function Create-StorageAccount {
+    param (
+        [string]$resourceGroupName,
+        [string]$location,
+        [string]$sku
+    )
+    $templateFilePath = "./storage_account_template.json"
+    $storageAccountName = "mysa" + (Get-Date -Format "yyyyMMddHHmmss")
+    New-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $storageAccountName -Location $location -SkuName $sku
+}
+
+
+
+Create-StorageAccount -resourceGroupName $resourceGroupName -location $location -sku $sku
 
 
