e64cdc26c05d41778a65c31b93ba1a6d22f4d40a
Dalia <dalya1812@gmail.com>
2023-08-12 22:37:52 +0300

    cleanup

--- Scripts/server.ps1
+++
@@ -1,5 +0,0 @@
-. "./vm_deployment.ps1"
-$resourceGroupName = "NewResourceGroup"
-$templateVMFilePath = "./vm_template.json"
-$vm = CreateVM -resourceGroupName $resourceGroupName -templateFilePath $templateVMFilePath -vmName $vmName
-$vm
